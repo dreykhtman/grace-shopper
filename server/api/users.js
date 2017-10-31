f5844f70fe4bbbf277173a258474a4fe8efd7019
Lucy Sutton <lucy.g.sutton@gmail.com>
2017-10-31 17:10:21 -0400

    working on user routes

--- server/api/users.js
+++ server/api/users.js
@@ -7,8 +7,14 @@ router.get('/', (req, res, next) => {
     // explicitly select only the id and email fields - even though
     // users' passwords are encrypted, it won't help if we just
     // send everything to anyone who asks!
-    attributes: ['id', 'email']
+    attributes: ['id', 'email', 'name', 'address']
   })
     .then(users => res.json(users))
     .catch(next)
 })
+
+router.get('/:userId', (req, res, next) => {
+  User.findById(req.params.userId, { attributes: ['id', 'email', 'name', 'address']})
+    .then(user => res.json(user))
+    .catch(next)
+});
