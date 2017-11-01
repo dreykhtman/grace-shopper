2a2649c405a8cf49888712a06690757ea6e37e87
Dima Reykhtman <neoxdth@gmail.com>
2017-11-01 11:20:04 -0400

    users route

--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const { User } = require('../db/models')
 module.exports = router
 
 router.get('/', (req, res, next) => {
@@ -12,3 +12,41 @@ router.get('/', (req, res, next) => {
     .then(users => res.json(users))
     .catch(next)
 })
+
+// get user by id
+router.get('/:id', (req, res, next) => {
+  let id = req.params.id;
+
+  User.findOne({
+    where: {
+      id
+    },
+    attributes: ['id', 'email', 'name', 'address', 'cc']
+  })
+    .then(user => res.json(user))
+    .catch(next);
+});
+
+
+router.post('/', (req, res, next) => {
+  User.create(req.body)
+    .then(user => res.status(201).json(user))
+    .catch(next);
+});
+
+// update user
+router.put('/:id', (req, res, next) => {
+  let id = req.params.id;
+  User.findById(id)
+    .then(user => user.update(req.body))
+    .then(user => res.json(user))
+    .catch(next);
+});
+
+router.delete('/:id', (req, res, next) => {
+  let id = req.params.id;
+
+  User.destroy({ where: { id } })
+    .then(() => res.status(204).end())
+    .catch(next);
+});
