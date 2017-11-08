1bbf206b45b76a174ef2ff3eb1e03df8cc1ba746
Jonathan Martinez <jonm90@gmail.com>
2017-11-08 03:20:10 -0500

    confirmTicket

--- client/components/OrderConfirmation.js
+++ client/components/OrderConfirmation.js
@@ -4,7 +4,7 @@ export const OrderConfirmation = () => {
   return (
     <div>
       <h2>Order Confirmation:</h2>
-      <p>Your order has been placed.</p>
+      <p>Your order has been placed. Check your registered email for a confirmation ticket</p>
     </div>
   )
 }
